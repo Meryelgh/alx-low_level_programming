3798cd4b2495295082509d24b0fc77c59d12cc6d
meryelgh <elghouti.meryam@gmail.com>
2023-02-16 08:03:32 -0800

    Task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
+#include <stdio.h> 
 /**
- * main - main block
- * Return: 0
+ * main - A program that prints a line with puts function
+ * Return: 0 (success)
  */
-
-int main(void)
+int main(void) 
 {
-       puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 } 
